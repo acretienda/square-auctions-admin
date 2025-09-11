cec24d0717bb41d86b5a4c57d373bb12131767ae
acretienda <acre.tienda@gmail.com>
2025-09-12 00:57:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   const token = localStorage.getItem("token");
 
   return (
     <Router>
+      <Navbar />
       <Routes>
-        {/* Ruta de login */}
         <Route path="/login" element={<Login />} />
-
-        {/* Ruta protegida */}
         <Route
           path="/dashboard"
           element={token ? <Dashboard /> : <Navigate to="/login" />}
         />
-
-        {/* Redirigir cualquier ruta desconocida al login */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
